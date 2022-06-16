9f8c975d8028ec3ad293f5dba44850ec76383649
Anil chauhan <44391555+anil-sookshum@users.noreply.github.com>
2022-06-16 18:00:59 +0530

    Update GoogleMapLocationPickerPlugin.kt

--- android/src/main/kotlin/com/humazed/google_map_location_picker/GoogleMapLocationPickerPlugin.kt
+++ android/src/main/kotlin/com/humazed/google_map_location_picker/GoogleMapLocationPickerPlugin.kt
@@ -1,57 +1,70 @@
 package com.humazed.google_map_location_picker
 
+import androidx.annotation.NonNull
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
-import java.security.NoSuchAlgorithmException
-import java.security.MessageDigest
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import android.content.pm.PackageManager
-import android.content.pm.PackageInfo
-import androidx.annotation.UiThread
-import android.app.Activity
 import java.math.BigInteger
+import java.security.MessageDigest
+import android.content.pm.PackageInfo
 
+class GoogleMapLocationPickerPlugin : FlutterPlugin, MethodCallHandler, ActivityAware  {
+    private lateinit var channel : MethodChannel
+    private var activityBinding: ActivityPluginBinding? = null
 
-class GoogleMapLocationPickerPlugin(act: Activity): MethodCallHandler {
-  var activity: Activity;
-
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "google_map_location_picker")
-      channel.setMethodCallHandler(GoogleMapLocationPickerPlugin(registrar.activity()))
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "google_map_location_picker")
+        channel.setMethodCallHandler(this)
     }
-  }
 
-  init {
-    activity = act;
-  }
-
-  @UiThread
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    }
-    if (call.method == "getSigningCertSha1") {
-        try {
-            val info: PackageInfo = activity.getPackageManager().getPackageInfo(call.arguments<String>(), PackageManager.GET_SIGNATURES)
-            for (signature in info.signatures) {
-                val md: MessageDigest = MessageDigest.getInstance("SHA1")
-                md.update(signature.toByteArray())
-
-                val bytes: ByteArray = md.digest()
-                val bigInteger: BigInteger = BigInteger(1, bytes)
-                val hex: String = String.format("%0" + (bytes.size shl 1) + "x", bigInteger)
-        
-                result.success(hex)
-            }
-        } catch (e: Exception) {
-            result.error("ERROR", e.toString(), null)
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if(activityBinding == null) {
+            result.notImplemented()
+            return
         }
-    } else {
-      result.notImplemented()
+        if (call.method == "getSigningCertSha1") {
+            try {
+                //.orEmpty() added on 14/05/22 by mg to call.arguments<String>().orEmpty()
+                val info: PackageInfo = activityBinding!!.activity.packageManager.getPackageInfo(call.arguments<String>().orEmpty(), PackageManager.GET_SIGNATURES)
+                for (signature in info.signatures) {
+                    val md: MessageDigest = MessageDigest.getInstance("SHA1")
+                    md.update(signature.toByteArray())
+
+                    val bytes: ByteArray = md.digest()
+                    val bigInteger = BigInteger(1, bytes)
+                    val hex: String = String.format("%0" + (bytes.size shl 1) + "x", bigInteger)
+
+                    result.success(hex)
+                }
+            } catch (e: Exception) {
+                result.error("ERROR", e.toString(), null)
+            }
+        } else {
+            result.notImplemented()
+        }
     }
-  }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activityBinding = binding
+    }
+
+    override fun onDetachedFromActivity() {
+        activityBinding = null
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    }
 }
